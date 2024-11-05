f1c38a59220aaf9221a3e479ba804c18c400419a
longbow122 <dhruvilp480@gmail.com>
2024-11-05 23:56:57 +0000

    Remove debug comments and old ID-assigning code.

--- data-model/src/main/java/me/longbow122/datamodel/repository/entities/Copypasta.java
+++ data-model/src/main/java/me/longbow122/datamodel/repository/entities/Copypasta.java
@@ -14,15 +14,6 @@ import lombok.Setter;
 @Table(name = "copypasta", indexes = @Index(columnList = "name"), uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class Copypasta {
 
-	//TODO NEED TO INVESTIGATE THIS AT SOME POINT. WE DO NOT NEED OR EVEN USE A LONG ID, BUT CONSTRAINTS SEEM TO BE BROKEN IN A STRANGE WAY WITHOUT THE ID.
-
-	//TODO WE HAVE MANAGED TO REMOVE THIS ID SUCCESSFULLY BUT WE HAVE DONE SO AT THE COST OF A SUPER HACKY IMPLEMENTATION
-	// BY OVERRIDING THE SAVE METHOD! HAVE A LOOK AND SEE WHAT NEEDS TO BE DONE ABOUT THIS AND WHETHER DOING THIS IS A GOOD IDEA...
-	// WHEN OVERRIDING THE SAVE METHOD, WE ARE DOING SO BY CALLING REPOSITORY-LAYER METHODS INSIDE THE REPOSITORY CLASS. FEELS LIKE BAD PRACTISE.
-	/*@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;*/
-
 	@Id
 	@Column(unique = true, nullable = false, length = 32)
 	@NotNull(message = "Name cannot be null")
