86fbf1734b1d724fdb11e1e75652d709fe1d1222
longbow122 <dhruvilp480@gmail.com>
2024-11-11 12:34:13 +0000

    Implement basic modal-form interaction.

--- bot/src/main/java/me/longbow122/bot/listener/CopypastaModalListener.java
+++ bot/src/main/java/me/longbow122/bot/listener/CopypastaModalListener.java
@@ -4,7 +4,9 @@ import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.longbow122.bot.dto.CopypastaDTO;
+import me.longbow122.bot.dto.FormDTO;
 import me.longbow122.bot.service.CopypastaService;
+import me.longbow122.bot.service.FormService;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
@@ -12,6 +14,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -19,6 +22,8 @@ import java.util.Objects;
 public class CopypastaModalListener extends ListenerAdapter {
 
 	private final CopypastaService copypastaService;
+
+	private final FormService formService;
 
 	/*
 	* We do not get exceptions or much control over invalid input at the Modal layer, so a lot of this validation does not end up triggering.
@@ -53,6 +58,20 @@ public class CopypastaModalListener extends ListenerAdapter {
 				}
 				event.getUser().openPrivateChannel().queue(channel -> channel.sendMessage(toSend).queue());
 			}
+		}
+		if (event.getModalId().equals("formSend")) {
+			FormDTO form = new FormDTO(event.getUser().getName(), 1L, List.of(
+				"What currency will you be purchasing in? What is your budget?",
+				"How would you prioritise form factor, build quality, performance and battery life? Do you have a preferred screen size?",
+				"Are you doing any CAD/Video Editing/Gaming? List which programs/games you would like to run.",
+				"Any specific requirements such as good keyboard, touch-screen, 2-in-1, fingerprint reader, optical drive, etc?",
+				"Leave any finishing thoughts here you feel are potentially necessary and beneficial to the discussion."), List.of(
+				Objects.requireNonNull(event.getValue("question1")).getAsString(),
+				Objects.requireNonNull(event.getValue("question2")).getAsString(),
+				Objects.requireNonNull(event.getValue("question3")).getAsString(),
+				Objects.requireNonNull(event.getValue("question4")).getAsString(),
+				Objects.requireNonNull(event.getValue("question5")).getAsString()));
+			formService.postForm(form);
 		}
 	}
 
